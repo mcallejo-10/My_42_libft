f0e75948d5e6e62ce411e7d38b4278099716c95f
mcallejo-10 <mcallejo@student.42barcelona.com>
2023-09-27 01:46:05 +0200

    dimarts nit 3

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: miranda <miranda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:40:47 by mcallejo          #+#    #+#             */
-/*   Updated: 2023/09/27 00:57:09 by miranda          ###   ########.fr       */
+/*   Updated: 2023/09/27 01:32:26 by miranda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,46 +69,3 @@ int	main(void)
 	ft_split(str, c);
 	return (0);
 }*/
-//&& (strlen(numstr) < ft_subsnum(s, c)))
-
-char **ft_split(const char *s, char c)
-{
-	char **ret;     // Declaración de un puntero a una matriz de cadenas de caracteres.
-	size_t i;       // Declaración de una variable para llevar un contador.
-	size_t len;     // Declaración de una variable para almacenar la longitud de cada subcadena.
-
-	if (!s)         // Verificación si el puntero de entrada 's' es nulo.
-		return (0); // Si 's' es nulo, la función devuelve un puntero nulo.
-
-	i = 0;          // Inicializa el contador 'i' a 0.
-	
-	// Se reserva espacio en la memoria para un arreglo de punteros a cadenas de caracteres
-	// (el número de elementos se determina llamando a la función ft_toklen con 's' y 'c').
-	ret = malloc(sizeof(char *) * (ft_toklen(s, c) + 1));
-	
-	if (!ret)       // Verificación si la asignación de memoria fue exitosa.
-		return (0); // Si no se pudo asignar memoria, la función devuelve un puntero nulo.
-	
-	while (*s)      // Bucle principal que recorre la cadena 's' hasta llegar al final.
-	{
-		if (*s != c) // Si el carácter actual no es igual al carácter delimitador 'c'.
-		{
-			len = 0; // Inicializa la variable 'len' a 0.
-
-			// Se cuenta la longitud de la subcadena hasta encontrar el siguiente carácter 'c'.
-			while (*s && *s != c)
-			{
-				++len;
-				++s;
-			}
-
-			// Se utiliza la función ft_substr para extraer la subcadena y se almacena en 'ret'.
-			ret[i++] = ft_substr(s - len, 0, len);
-		}
-		else
-			++s; // Si el carácter actual es igual a 'c', se pasa al siguiente carácter.
-	}
-
-	ret[i] = 0;     // Se agrega un puntero nulo al final del arreglo de cadenas.
-	return (ret);   // La función devuelve el arreglo de cadenas de caracteres resultante.
-}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,30 @@
 /*   By: miranda <miranda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:02:22 by mcallejo          #+#    #+#             */
-/*   Updated: 2023/09/27 01:28:18 by miranda          ###   ########.fr       */
+/*   Updated: 2023/09/27 01:44:13 by miranda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {   
-    int     i;
-    int     j;
-    size_t  setlen;
-    size_t  len;
-    char    *ret;
+	unsigned int	i;
+	size_t			setlen;
+	size_t			len;
+	char			*ret;
 
-    setlen = ft_strlen(set);
-    len = ft_strlen(s1);
+	setlen = ft_strlen(set);
+	len = ft_strlen(s1);
 	if (!s1)
-        return (NULL);
-    if (!set)
-        return (s1);
-    while (*s1 && (ft_strnstr(set, s1[i], setlen) != NULL))
-               i++;
-    while (len > 0 && (ft_strnstr(set, s1[len], setlen) != NULL))
-        len--;
-    ret = malloc(sizeof(char) * i - (int *)len);
-    ft_strlcpy(s1, i, len - i);
-    return (ret);
+		return ('\0');
+	if (!set)
+		return ((char *)s1);
+	while (*s1 && (ft_strnstr(set, s1 + i, setlen) != NULL))
+			   i++;
+	while (len > 0 && (ft_strnstr(set, s1 + len, setlen) != NULL))
+		len--;
+	ret = malloc(sizeof(char) * (i -len));
+	ft_strlcpy((char *)s1, ret, len - i);
+	return (ret);
 }
